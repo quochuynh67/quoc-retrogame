dfa1ecd8608fcb048af49a8087ae6162bbd74917
quoc.huynh <stacktecg@stacktecgs-MacBook-Air.local>
2026-05-19 13:41:18 +0700

    feat: increase storage list limit to 500 in migration script

--- migrate_vlogs.js
+++ migrate_vlogs.js
@@ -59,7 +59,7 @@ async function migrate() {
       },
       body: JSON.stringify({
         prefix: '',
-        limit: 100,
+        limit: 500,
         offset: 0,
         sortBy: {
           column: 'name',
